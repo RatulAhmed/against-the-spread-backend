e3183befcc44ed66a1cd86bb7146f94d3b39ffec
ratulahmed <ratulahmed1@gmail.com>
2020-08-18 19:38:42 -0400

    Successfully pulled first spread

---
+++ src/main/java/com/atspickem/backend/util/OddsScraper.java
@@ -0,0 +1,30 @@
+package com.atspickem.backend.util;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class OddsScraper {
+
+    public static void main(String[] args) {
+        String oddsUrl = "https://sportsbook.draftkings.com/leagues/football/3?category=game-lines&subcategory=game";
+        Document doc = null;
+
+        {
+            try {
+                doc = Jsoup.connect(oddsUrl).userAgent("Mozilla").referrer("http://google.com").get();
+            } catch (IOException e) {
+                System.out.println("Error in connecting to odds url");
+            }
+        }
+        Elements tableData = doc.getElementsByClass("sportsbook-outcome-cell");
+        String result = tableData.toString();
+        Element spread = tableData.first().child(0);
+        Elements outcomeCell = spread.select("div.sportsbook-outcome-cell__body[aria-label]");
+        String ariaLabel = outcomeCell.attr("aria-label");
+        System.out.println(ariaLabel);
+    }
+}
