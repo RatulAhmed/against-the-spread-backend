1cb54a122966d25728255102e02fc3702e0469b6
ratulahmed <ratulahmed1@gmail.com>
2020-08-18 20:15:37 -0400

    Scared all odds - incluindg over/unders WIP

--- src/main/java/com/atspickem/backend/util/OddsScraper.java
+++ src/main/java/com/atspickem/backend/util/OddsScraper.java
@@ -6,6 +6,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.List;
 
 public class OddsScraper {
 
@@ -20,11 +21,23 @@ public class OddsScraper {
                 System.out.println("Error in connecting to odds url");
             }
         }
-        Elements tableData = doc.getElementsByClass("sportsbook-outcome-cell");
-        String result = tableData.toString();
-        Element spread = tableData.first().child(0);
-        Elements outcomeCell = spread.select("div.sportsbook-outcome-cell__body[aria-label]");
-        String ariaLabel = outcomeCell.attr("aria-label");
-        System.out.println(ariaLabel);
+//        Elements tableData = doc.getElementsByClass("sportsbook-outcome-cell");
+//        String result = tableData.toString();
+//        Element spread = tableData.first().child(0);
+//        Elements outcomeCell = spread.select("div.sportsbook-outcome-cell__body[aria-label]");
+//        String ariaLabel = outcomeCell.attr("aria-label");
+//        System.out.println(ariaLabel);
+
+        List<Element> oddsDiv = null;
+        String odds = null;
+        Elements parentDiv = doc.getElementsByClass("sportsbook-outcome-cell");
+        Elements test = parentDiv.select("div.sportsbook-outcome-cell__body[aria-label]");
+//        System.out.println(test.toString());
+
+        for(Element e1 : test) {
+            odds = e1.attr("aria-label");
+            System.out.println(odds);
+        }
+
     }
 }
