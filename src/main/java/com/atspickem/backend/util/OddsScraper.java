e4955693f1891f61a80dc909d52c1e01f7a2ad3a
ratulahmed <ratulahmed1@gmail.com>
2020-08-18 20:27:28 -0400

    Getting all spreads, filtered out over/unders

--- src/main/java/com/atspickem/backend/util/OddsScraper.java
+++ src/main/java/com/atspickem/backend/util/OddsScraper.java
@@ -21,14 +21,23 @@ public class OddsScraper {
                 System.out.println("Error in connecting to odds url");
             }
         }
+//        Elements tableData = doc.getElementsByClass("sportsbook-outcome-cell");
+//        String result = tableData.toString();
+//        Element spread = tableData.first().child(0);
+//        Elements outcomeCell = spread.select("div.sportsbook-outcome-cell__body[aria-label]");
+//        String ariaLabel = outcomeCell.attr("aria-label");
+//        System.out.println(ariaLabel);
 
         List<Element> oddsDiv = null;
         String odds = null;
         Elements parentDiv = doc.getElementsByClass("sportsbook-outcome-cell");
         Elements test = parentDiv.select("div.sportsbook-outcome-cell__body[aria-label]");
+//        System.out.println(test.toString());
 
         for(Element e1 : test) {
             odds = e1.attr("aria-label");
+
+            if(!(odds.startsWith("O") || odds.startsWith("U")))
             System.out.println(odds);
         }
     }
