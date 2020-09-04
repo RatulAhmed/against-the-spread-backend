0c575ebf40c1c1838116b079208ed172613e539b
ratulahmed <ratulahmed1@gmail.com>
2020-09-03 23:22:51 -0400

    Created UserChoice model that represents Joined DB table

--- src/main/java/com/atspickem/backend/models/NflSpread.java
+++ src/main/java/com/atspickem/backend/models/NflSpread.java
@@ -1,11 +1,9 @@
 package com.atspickem.backend.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "nfl_spread")
 public class NflSpread {
 
     @Id
---
+++ src/main/java/com/atspickem/backend/models/UserChoice.java
@@ -0,0 +1,58 @@
+package com.atspickem.backend.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="user_choice")
+public class UserChoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private int userId;
+    private int spreadId;
+    private char choice;
+
+    public UserChoice() {
+    }
+
+    public UserChoice(String id, int userId, int spreadId, char choice) {
+        this.id = id;
+        this.userId = userId;
+        this.spreadId = spreadId;
+        this.choice = choice;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getSpreadId() {
+        return spreadId;
+    }
+
+    public void setSpreadId(int spreadId) {
+        this.spreadId = spreadId;
+    }
+
+    public char getChoice() {
+        return choice;
+    }
+
+    public void setChoice(char choice) {
+        this.choice = choice;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Id
+    public String getId() {
+        return id;
+    }
+}
